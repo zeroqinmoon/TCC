ee444490c5f788521e13e4dc93d62db9ff44e4c1
ZeroQinMoon <165333875+zeroqinmoon@users.noreply.github.com>
2024-07-28 14:39:35 -0300

    comment and documentation of code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
+const express = require('express'); // importa o express
+const nodemailer = require('nodemailer'); // importa o nodemailer
+const bodyParser = require('body-parser'); // importa o body-parser
 
+// configura a inicializacao
 const app = express();
 const port = 3000;
 
@@ -35,30 +36,30 @@ nodemailer.createTestAccount((err, account) => {
 
         // Configurar objeto da mensagem
         let mailOptions = {
-            from: 'Sender Name <sender@example.com>', // Substitua pelo seu endereço de remetente
-            to: 'Recipient <recipient@example.com>',   // Substitua pelo endereço do destinatário
-            subject: 'Envio de Formulário de Contato',
-            text: `Nome: ${name}\nMensagem: ${message}`,
-            html: `<p><strong>Nome:</strong> ${name}</p><p><strong>Mensagem:</strong> ${message}</p>`
+            from: 'Sender Name <sender@example.com>', // Substitua pelo seu endereço de remetente(opcional)
+            to: 'Recipient <recipient@example.com>',   // Substitua pelo endereço do destinatário(opcional)
+            subject: 'Envio de Formulário de Contato', // Objetivo da mensagem
+            text: `Nome: ${name}\nMensagem: ${message}`, // Recebe a mensagem do html via post
+            html: `<p><strong>Nome:</strong> ${name}</p><p><strong>Mensagem:</strong> ${message}</p>` // Recebe o texto do html via post
         };
 
         // Enviar e-mail
         transporter.sendMail(mailOptions, (err, info) => {
             if (err) {
-                console.log('Ocorreu um erro. ' + err.message);
+                console.log('Ocorreu um erro. ' + err.message); // auto explicativo
                 return res.status(500).json({ success: false, message: 'Erro ao enviar o e-mail.' });
             }
 
-            console.log('Mensagem enviada: %s', info.messageId);
+            console.log('Mensagem enviada: %s', info.messageId); // auto explicativo
             console.log('URL de visualização: %s', nodemailer.getTestMessageUrl(info));
             res.status(200).json({ success: true, message: 'E-mail enviado com sucesso.' });
         });
     });
 
-    // Servir a página HTML estática
+    // Servir a página HTML estática, resumo: cria uma pagina para verificar se enviou ou nao
     app.use(express.static('public'));
 
     app.listen(port, () => {
-        console.log(`Servidor rodando em http://localhost:${port}`);
+        console.log(`Servidor rodando em http://localhost:${port}`); // mostra o link do email enviado no sistema de testes.
     });
 });
